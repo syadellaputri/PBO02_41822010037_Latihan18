bd51c5c722f4454311d560d39488d1be45e47178
Syadellaputri <146219177+syadellaputri@users.noreply.github.com>
2023-09-28 23:14:15 +0700

    Add files via upload

---
+++ PBO02-41822010037-Latihan18/src/main/java/app/pbo02/latihan18/PBO0241822010037Latihan18.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app.pbo02.latihan18;
+
+/**
+ *
+ * @author DelDel
+ * Nama : Syadella Putri
+ * NIM  : 41822010037
+ * Nama : Anastasya Azami
+ * NIM  : 41822010081
+ */
+public class PBO0241822010037Latihan18 {
+
+    public static void main(String[] args) {
+        System.out.println("Format Latihan PBO");
+        
+        double x=3.5;
+        while(x<=35){
+            System.out.println(x);
+            x+=3.5;
+        }
+    }
+}
